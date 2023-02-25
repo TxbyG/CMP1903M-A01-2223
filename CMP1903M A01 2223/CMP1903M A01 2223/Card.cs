3a85806a209c9695fc70030fd3371c4270ca4388
TxbyG <125763262+TxbyG@users.noreply.github.com>
2023-02-25 11:45:54 +0000

    Cards update
    Cards script

--- CMP1903M A01 2223/CMP1903M A01 2223/Card.cs
+++ CMP1903M A01 2223/CMP1903M A01 2223/Card.cs
@@ -10,9 +10,21 @@ namespace CMP1903M_A01_2223
     {
         //Base for the Card class.
         //Value: numbers 1 - 13
+        int[] valueNumbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+
         //Suit: numbers 1 - 4
+        int[] suitNumbers = new int[] { 1, 2, 3, 4 };
         //The 'set' methods for these properties could have some validation
-        public int Value { get; set; }
-        public int Suit { get; set; }
+
+        public int Value 
+        { 
+            get { return Value; }
+            set { Value = (int)valueNumbers.GetValue(1, 4);  }
+        }
+
+        public int Suit {
+            get { return Suit; }
+            set { Suit = (int)suitNumbers.GetValue(1, 4); } 
+        }
     }
 }
